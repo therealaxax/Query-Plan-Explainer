f5e556aaf6635b4b2f49856994598affa65a1bd7
Bob Lin <boblinbob0906@gmail.com>
2022-10-29 21:17:37 +0800

    added psycopg2

--- annotations.py
+++ annotations.py
@@ -1,11 +1,56 @@
-import preprocessing
+from re import 
+import psycopg2
+import textwrap
+import re
+ 
+ # Postgres TCP-H database details
+DB_NAME = "TPC-H"
+DB_USER = "postgres"
+DB_PASS = "123 "
+DB_HOST = "localhost"
+DB_PORT = "5432"
 
-def annotate(select_text, from_text, where_text):
+def explain(select_text, from_text, where_text):
 
-    # Call preprocessing() to preprocess user input
-    processed = preprocessing.process()
+    # Connect to postgres database
+    conn = psycopg2.connect(database=DB_NAME,
+                            user=DB_USER,
+                            password=DB_PASS,
+                            host=DB_HOST,
+                            port=DB_PORT)
+    print("Database connected successfully")
 
-    # Perform algo to find query plan steps 
-    # EXPLAIN command
+    # Construct SQL query text to EXPLAIN
+    query = select_text + '\n' + from_text + '\n' + where_text
 
-    # return annotations
+    # Execute EXPLAIN command on user's input query 
+    explain_command = 'EXPLAIN VERBOSE ' + query
+    cur = conn.cursor()
+    cur.execute(explain_command)
+    data = str(cur.fetchall())
+    print(data)
+    # data = textwrap.wrap(data, 6)
+    print('Data fetched successfully')
+    conn.close()
+
+    # Identify tables mentioned in query
+    tables_text = from_text.strip('FROM ')
+    tables = tables_text.split(',')
+    print('Tables are: ', tables)
+    
+    # Find the info pertaining to each table, then return this value to be displayed!
+    output = []
+    for table_name in tables:
+        search_string = "'" + '(.+?)' + 'on public.' + table_name
+
+        m = re.search(search_string, data)
+        if m:
+            found = m.group(1)
+            print(found + 'on public.' + table_name)
+            output.append(found + 'on public.' + table_name)
+
+
+    # Identify joins (if any)
+
+    print(output)
+    return data, output
--- interface.py
+++ interface.py
@@ -1,6 +1,9 @@
+# from __future__ import annotations
 import PySimpleGUI as sg
 import os.path
 import io
+
+import annotations
 
 sg.theme('DarkTeal12')
 
@@ -9,22 +12,26 @@ def display():
     layout = [
         [
             # [sg.Image('logo.png', size=(50,50))],
-            [sg.Text('Please enter your SQL query')],
-            [sg.Text('SELECT Statement', size =(25, 1)), sg.InputText(key='-SELECT-')],
-            [sg.Text('FROM Statement', size =(25, 1)), sg.InputText(key='-FROM-')],
-            [sg.Text('WHERE Statement', size =(25, 1)), sg.InputText(key='-WHERE-')],
-            [sg.Button("Send", button_color=('Black', 'LightBlue'))],
+            [sg.Text('Please enter your SQL query\n', font=('Helvetica', 20))],
+            [sg.Text('SELECT Statement', font=('Helvetica', 14), size =(25, 1)), sg.InputText(key='-SELECT-', font=('Helvetica', 14))],
+            [sg.Text('FROM Statement', font=('Helvetica', 14), size =(25, 1)), sg.InputText(key='-FROM-', font=('Helvetica', 14))],
+            [sg.Text('WHERE Statement', font=('Helvetica', 14), size =(25, 1)), sg.InputText(key='-WHERE-', font=('Helvetica', 14))],
+            [sg.Text('')],
+            [sg.Button("Send", font=('Helvetica', 14), button_color=('Black', 'LightBlue'))],
             [sg.Text('')],
             [sg.Text('')],
             [sg.Text('')],
-            [sg.Text('The statement is:')],
-            [sg.Text(size=(50, 2), font=('Helvetica', 20), justification='left', key='-DISPLAY_SELECT-')],
-            [sg.Text(size=(50, 2), font=('Helvetica', 20), justification='left', key='-DISPLAY_FROM-')],
-            [sg.Text(size=(50, 2), font=('Helvetica', 20), justification='left', key='-DISPLAY_WHERE-')]
+            [sg.Text(font=('Helvetica', 14), key='-DISPLAY_MSG-')],
+            [sg.Text(size=(50, 2), font=('Helvetica', 18), justification='left', key='-DISPLAY_SELECT-')],
+            [sg.Text(size=(50, 2), font=('Helvetica', 18), justification='left', key='-DISPLAY_FROM-')],
+            [sg.Text(size=(50, 2), font=('Helvetica', 18), justification='left', key='-DISPLAY_WHERE-')],
+            [sg.Text(size=(100, 20), font=('Helvetica', 14), justification='left', key='-RESULTS-')],
+            [sg.Text('')],
+            [sg.Text(size=(100, 100), font=('Helvetica', 14), justification='left', key='-OUTPUT-')]
         ]
     ]
 
-    window = sg.Window("Query Plan Explainer", layout, size=(500, 500))
+    window = sg.Window("Query Plan Explainer", layout, size=(800, 800))
 
     while True:
         event, values = window.read()
@@ -35,17 +42,20 @@ def display():
             select_text = values['-SELECT-']
             from_text = values['-FROM-']
             where_text = values['-WHERE-']
-            print(select_text, from_text, where_text)
-            
-            # TODO
-            # This is the part to call annotation.py with the parameters of the user input SQL text
-            # And get back the return values from annotations.py
 
-            # Use returned values to label the frontend
-
+            window['-DISPLAY_MSG-'].update('The query to explain is: ')
             window['-DISPLAY_SELECT-'].update(select_text)
             window['-DISPLAY_FROM-'].update(from_text)
             window['-DISPLAY_WHERE-'].update(where_text)
+            print(select_text, from_text, where_text)
+            
+            # This is the part to call annotation.py with the parameters of the user input SQL text
+            # And get back the return values from annotations.py
+            raw_data, output = annotations.explain(select_text, from_text, where_text)
+
+            # Use returned values to label the frontend
+            window['-RESULTS-'].update(raw_data)
+            window['-OUTPUT-'].update(str(output))
         
     
     
